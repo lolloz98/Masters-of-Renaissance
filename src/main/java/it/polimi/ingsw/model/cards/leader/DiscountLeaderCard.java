2198f9d2713913fc554a9dead51dd123f7aa8c5e
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-03-29 19:11:38 +0200

    InvalidResourcesByPlayerException added. Resource.TRANSFORM removed.

--- src/main/java/it/polimi/ingsw/model/cards/leader/DiscountLeaderCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/leader/DiscountLeaderCard.java
@@ -1,11 +1,30 @@
 package it.polimi.ingsw.model.cards.leader;
 
 import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.game.Resource;
 import it.polimi.ingsw.model.player.Player;
 
 public final class DiscountLeaderCard extends LeaderCard<RequirementColorsDevelop> {
-    public DiscountLeaderCard(int victoryPoints, RequirementColorsDevelop requirement) {
+    private final Resource res;
+    private final int quantity = 1;
+
+    public DiscountLeaderCard(int victoryPoints, RequirementColorsDevelop requirement, Resource res) {
         super(victoryPoints, requirement);
+        this.res = res;
+    }
+
+    /**
+     * @return type of resource to discount
+     */
+    public Resource getRes() {
+        return res;
+    }
+
+    /**
+     * @return amount of resources to discount
+     */
+    public int getQuantity() {
+        return quantity;
     }
 
     @Override
