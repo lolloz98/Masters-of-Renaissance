d78ff7fe46da480c548864be2ac44449a482ed54
lorenzoinnocenti <innocentilorenzo@hotmail.it>
2021-04-03 18:01:02 +0200

    Add performLorenzoAction in Lorenzo

--- src/main/java/it/polimi/ingsw/model/game/Lorenzo.java
+++ src/main/java/it/polimi/ingsw/model/game/Lorenzo.java
@@ -1,13 +1,14 @@
 package it.polimi.ingsw.model.game;
 
+import it.polimi.ingsw.model.exception.EmptyDeckException;
 import it.polimi.ingsw.model.player.FaithTrack;
 
 public class Lorenzo {
     private int pathNumber;
     private final FaithTrack faithTrack;
 
-    public Lorenzo(FaithTrack faithTrack) {
-        this.faithTrack = faithTrack;
+    public Lorenzo() {
+        this.faithTrack = new FaithTrack();
     }
 
     public int getPathNumber() {
@@ -18,5 +19,12 @@ public class Lorenzo {
         return faithTrack;
     }
 
-
+    /**
+     * Draws a card from the LorenzoDeck in SinglePlayer and activates its effect
+     *
+     * @throws EmptyDeckException if deck is empty
+     */
+    public void performLorenzoAction(SinglePlayer singlePlayer){
+        singlePlayer.getLorenzoDeck().drawCard().applyEffect(singlePlayer);
+    }
 }
