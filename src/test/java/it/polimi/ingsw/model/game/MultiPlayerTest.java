7a8167a4c3b9ee289c7bc97dec479e97ca5ba544
lorenzoinnocenti <innocentilorenzo@hotmail.it>
2021-04-08 15:25:57 +0200

    Fix MultiPlayerTest

--- src/test/java/it/polimi/ingsw/model/game/MultiPlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/game/MultiPlayerTest.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.model.cards.Deck;
 import it.polimi.ingsw.model.cards.DeckDevelop;
 import it.polimi.ingsw.model.cards.DevelopCard;
 import it.polimi.ingsw.model.cards.leader.LeaderCard;
+import it.polimi.ingsw.model.cards.leader.Requirement;
+import it.polimi.ingsw.model.exception.EmptyDeckException;
 import it.polimi.ingsw.model.player.Player;
 import org.junit.Before;
 import org.junit.Test;
@@ -44,9 +46,50 @@ public class MultiPlayerTest {
     @Test
     public void testDeckLeader(){ // FIXME should be modified to exclude shuffle method
         // TODO test more
-        Deck<LeaderCard> dl = multiPlayer.getDeckLeader();
+        Deck<LeaderCard<? extends Requirement>> dl = multiPlayer.getDeckLeader();
         for(int i=0; i<12; i++){
             dl.drawCard();
         }
     }
+
+    @Test (expected = EmptyDeckException.class)
+    public void testIsADeckDevelopEmptyTooManyCalls(){
+        for(int i = 0; i <5; i++)
+            multiPlayer.getDecksDevelop().get(Color.BLUE).get(2).drawCard();
+    }
+
+    @Test
+    public void testIsADeckDevelopEmptyTrue(){
+        for(int i = 0; i <4; i++)
+            multiPlayer.getDecksDevelop().get(Color.BLUE).get(2).drawCard();
+        assertTrue(multiPlayer.isADeckDevelopEmpty());
+    }
+
+    @Test
+    public void testIsADeckDevelopEmptyFalse(){
+        for(int i = 0; i <3; i++)
+            multiPlayer.getDecksDevelop().get(Color.BLUE).get(2).drawCard();
+        assertTrue(!multiPlayer.isADeckDevelopEmpty());
+    }
+
+    @Test
+    public void testGetNewId(){
+        ArrayList<Player> players1 = new ArrayList<>();
+        players1.add(new Player("fourth", 1));
+        players1.add(new Player("fifth", 2));
+        players1.add(new Player("sixth", 3));
+        MultiPlayer multiPlayer1 = new MultiPlayer(players1);
+        ArrayList<Player> players2 = new ArrayList<>();
+        players2.add(new Player("Aniello", 1));
+        players2.add(new Player("Lorenzo", 2));
+        players2.add(new Player("Lorenzo", 3));
+        MultiPlayer multiPlayer2 = new MultiPlayer(players2);
+        assertNotEquals(multiPlayer2.getId(), multiPlayer1.getId());
+        assertNotEquals(multiPlayer1.getId(), multiPlayer.getId());
+        assertNotEquals(multiPlayer.getId(), multiPlayer2.getId());
+        multiPlayer1.destroy();
+        multiPlayer2.destroy();
+    }
+
+
 }
