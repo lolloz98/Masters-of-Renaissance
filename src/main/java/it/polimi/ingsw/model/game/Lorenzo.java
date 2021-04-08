828ad07f4be4a8d5fa596bd9b8ed31ca277c9f71
lorenzoinnocenti <innocentilorenzo@hotmail.it>
2021-04-08 11:49:36 +0200

    Removed useless attribute and method

--- src/main/java/it/polimi/ingsw/model/game/Lorenzo.java
+++ src/main/java/it/polimi/ingsw/model/game/Lorenzo.java
@@ -4,15 +4,10 @@ import it.polimi.ingsw.model.exception.EmptyDeckException;
 import it.polimi.ingsw.model.player.FaithTrack;
 
 public class Lorenzo {
-    private int pathNumber;
     private final FaithTrack faithTrack;
 
     public Lorenzo() {
         this.faithTrack = new FaithTrack();
-    }
-
-    public int getPathNumber() {
-        return pathNumber;
     }
 
     public FaithTrack getFaithTrack() {
