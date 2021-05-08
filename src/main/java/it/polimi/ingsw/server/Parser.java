e37991fae1b99c5640ceb918309ec77bff3dd399
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-05-08 17:46:00 +0200

    small changes to the Parser

--- src/main/java/it/polimi/ingsw/server/Parser.java
+++ src/main/java/it/polimi/ingsw/server/Parser.java
@@ -15,17 +15,20 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Set;
+import java.util.SplittableRandom;
 import java.util.stream.Collectors;
 
 public final class Parser {
     private static final Logger logger = LogManager.getLogger(Parser.class);
+    private static final String packageName = "it.polimi.ingsw.server.controller.messagesctr";
 
     public static Object parse(ClientMessage clientMessage) throws ControllerException{
         // Get all classes in the package with controller Requests
-        Reflections reflections = new Reflections("it.polimi.ingsw.server.controller.messagesctr");
+        Reflections reflections = new Reflections(packageName);
         Set<String> allClasses = reflections.getStore().values("SubTypesScanner");
 
-        List<String> controllerClassName =  allClasses.stream().filter(x -> x.contains(clientMessage.getClass().getSimpleName())).collect(Collectors.toList());
+        List<String> controllerClasses = allClasses.stream().filter(x -> x.contains(packageName)).collect(Collectors.toList());
+        List<String> controllerClassName =  controllerClasses.stream().filter(x -> x.endsWith(clientMessage.getClass().getSimpleName() + "Controller")).collect(Collectors.toList());
         if(controllerClassName.size() != 1) {
             logger.error("for the clientMessage we got " + controllerClassName.size() + " controllerMessages corresponding");
             throw new ControllerException("unable to parse the request");
