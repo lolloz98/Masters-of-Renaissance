24de36866f14fc6831bedf008ab57d800ab5015b
Aniello98 <aniello.desantis@mail.polimi.it>
2021-05-13 23:22:50 +0200

    implemented UseMarketMessageController

---
+++ src/main/java/it/polimi/ingsw/messages/answers/mainactionsanswer/UseMarketAnswer.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.messages.answers.mainactionsanswer;
+
+import it.polimi.ingsw.messages.answers.Answer;
+import it.polimi.ingsw.server.model.game.Resource;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class UseMarketAnswer extends Answer {
+    private final ArrayList<TreeMap<Resource, Integer>> resCombinations;
+
+
+    /**
+     * @param gameId   current game id
+     * @param playerId id of the player who sent the request
+     * @param resCombinations combinations of resources that the player can choose
+     */
+    public UseMarketAnswer(int gameId, int playerId, ArrayList<TreeMap<Resource, Integer>> resCombinations) {
+        super(gameId, playerId);
+        this.resCombinations = resCombinations;
+    }
+
+    public ArrayList<TreeMap<Resource, Integer>> getResCombinations() {
+        return resCombinations;
+    }
+}
--- src/main/java/it/polimi/ingsw/server/controller/messagesctr/playing/UseMarketMessageController.java
+++ src/main/java/it/polimi/ingsw/server/controller/messagesctr/playing/UseMarketMessageController.java
@@ -1,19 +1,54 @@
 package it.polimi.ingsw.server.controller.messagesctr.playing;
 
 import it.polimi.ingsw.messages.answers.Answer;
-import it.polimi.ingsw.messages.requests.ClientMessage;
+import it.polimi.ingsw.messages.answers.mainactionsanswer.UseMarketAnswer;
 import it.polimi.ingsw.messages.requests.actions.UseMarketMessage;
 import it.polimi.ingsw.server.controller.ControllerActions;
 import it.polimi.ingsw.server.controller.exception.ControllerException;
+import it.polimi.ingsw.server.controller.exception.UnexpectedControllerException;
+import it.polimi.ingsw.server.model.exception.MainActionAlreadyOccurredException;
+import it.polimi.ingsw.server.model.exception.MarketTrayNotEmptyException;
+import it.polimi.ingsw.server.model.exception.MatrixIndexOutOfBoundException;
+import it.polimi.ingsw.server.model.exception.ProductionsResourcesNotFlushedException;
+import it.polimi.ingsw.server.model.game.Game;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-//todo
+
 public class UseMarketMessageController extends PlayingMessageController{
+
+    private static final Logger logger = LogManager.getLogger(UseMarketMessageController.class);
+
     public UseMarketMessageController(UseMarketMessage clientMessage) {
         super(clientMessage);
     }
 
     @Override
     protected Answer doActionNoChecks(ControllerActions<?> controllerActions) throws ControllerException {
-        return null;
+        Game<?> game= controllerActions.getGame();
+
+        try {
+            game.getTurn().setMarketActivated(true);
+        } catch (MainActionAlreadyOccurredException e) {
+            throw new ControllerException("you have already done your main action");
+        } catch (MarketTrayNotEmptyException e) {
+            logger.error("the market tray is not empty and i'm trying to send "+this.getClass());
+            throw new UnexpectedControllerException("the market is not empty");
+        } catch (ProductionsResourcesNotFlushedException e) {
+            logger.error("there is resources not flushed while sending "+this.getClass());
+            throw new UnexpectedControllerException("the productions are not flushed");
+        }
+
+        UseMarketMessage clientMessage=(UseMarketMessage) getClientMessage();
+        try {
+            game.getMarketTray().pushMarble(clientMessage.isOnRow(),clientMessage.getIndex());
+        } catch (MarketTrayNotEmptyException e) {
+            logger.error("the market is not empty and i'm trying to push the marble ");
+            throw new UnexpectedControllerException("the market is not empty");
+        } catch (MatrixIndexOutOfBoundException e) {
+            throw new ControllerException("you have put a wrong matrix index, try again");
+        }
+
+        return new UseMarketAnswer(clientMessage.getGameId(),clientMessage.getPlayerId(),game.getMarketTray().getResCombinations());
     }
 }
