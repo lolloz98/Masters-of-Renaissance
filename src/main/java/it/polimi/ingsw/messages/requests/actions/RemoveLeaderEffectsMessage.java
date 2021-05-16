082469c6bcac7713c4d1f7c45bcc4c1753d63fb8
Lorenzo Innocenti <74833935+lorenzoinnocenti@users.noreply.github.com>
2021-05-16 22:51:58 +0200

    Removed apply and remove leader effect message

--- src/main/java/it/polimi/ingsw/messages/requests/actions/RemoveLeaderEffectsMessage.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.messages.requests.actions;
-
-import it.polimi.ingsw.messages.requests.ClientMessage;
-
-public class RemoveLeaderEffectsMessage extends ClientMessage {
-    public RemoveLeaderEffectsMessage(int gameId, int playerId) {
-        super(gameId, playerId);
-    }
-}
