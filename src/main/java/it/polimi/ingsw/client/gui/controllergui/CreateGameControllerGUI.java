f06ef707cc7ea2601dcdbf9d46a1d9fc2f758c21
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-05-22 02:03:53 +0200

    Start GUI and resources added

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllergui/CreateGameControllerGUI.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.client.gui.controllergui;
+
+import javafx.scene.control.TextField;
+
+public class CreateGameControllerGUI {
+    public TextField playerNumbers;
+    public TextField nickname;
+}
---
+++ src/main/java/it/polimi/ingsw/client/gui/controllergui/StartRemote.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.gui.controllergui;
+
+public class StartRemote {
+}
