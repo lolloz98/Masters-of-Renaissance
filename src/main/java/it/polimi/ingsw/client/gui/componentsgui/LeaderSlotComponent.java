ee89977811146995c5f1bf49347b0e3428bd0e59
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-05-23 15:17:27 +0200

    done leaderSlot

---
+++ src/main/java/it/polimi/ingsw/client/gui/componentsgui/LeaderSlotComponent.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.client.gui.componentsgui;
+
+import it.polimi.ingsw.client.localmodel.localcards.LocalCard;
+import it.polimi.ingsw.client.localmodel.localcards.LocalDevelopCard;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class LeaderSlotComponent extends AnchorPane {
+    private static final Logger logger = LogManager.getLogger(SlotDevelopComponent.class);
+
+    @FXML
+    private ImageView cardImg;
+    @FXML
+    private Button activateBtn;
+    @FXML
+    private Button discardBtn;
+
+    private LocalCard leaderCard;
+
+    public LocalCard getLeaderCard() {
+        return leaderCard;
+    }
+
+    public void setCard(LocalCard card){
+        this.leaderCard = card;
+        // Todo update activeCard imageView
+    }
+
+    public LeaderSlotComponent() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/fxml/board/leader_slot.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
--- src/main/java/it/polimi/ingsw/client/gui/controllergui/BoardControllerGUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllergui/BoardControllerGUI.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.client.gui.componentsgui.FaithTrackComponent;
 import it.polimi.ingsw.client.gui.componentsgui.SlotDevelopComponent;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class BoardControllerGUI implements ControllerGUI {
@@ -13,6 +14,12 @@ public class BoardControllerGUI implements ControllerGUI {
     public SlotDevelopComponent slotDevelopComponent1;
     public SlotDevelopComponent slotDevelopComponent2;
     public SlotDevelopComponent slotDevelopComponent3;
+    public Button optional1Btn;
+    public Button optional2Btn;
+    public Button marketBtn;
+    public Button developBtn;
+    public Button flushBtn;
+    public Label messageLbl;
 
     @Override
     public void setUp(Stage stage, Parent root, GUI ui) {
