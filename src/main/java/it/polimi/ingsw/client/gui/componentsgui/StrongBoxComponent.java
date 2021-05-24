d29bbe43499ad6567baf48cc483848c597bd3ab0
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-05-24 20:42:02 +0200

    small changes on StrongBoxComponent

--- src/main/java/it/polimi/ingsw/client/gui/componentsgui/StrongBoxComponent.java
+++ src/main/java/it/polimi/ingsw/client/gui/componentsgui/StrongBoxComponent.java
@@ -13,25 +13,30 @@ public class StrongBoxComponent extends Pane {
 
     public VBox vBox;
 
-    public void updateRes(TreeMap<Resource, Integer> strongboxRes){
-        for(Resource r: strongboxRes.keySet()){
+    public void updateRes(TreeMap<Resource, Integer> strongboxRes) {
+        for (Resource r : strongboxRes.keySet()) {
             updateRes(r, strongboxRes.get(r));
         }
     }
 
     public void updateRes(Resource r, Integer q) {
         boolean found = false;
-        for(Node c: vBox.getChildren()){
+        // remove element if q == 0
+        Node toBeRemoved = null;
+        for (Node c : vBox.getChildren()) {
             ResAmountComponent tmp = (ResAmountComponent) c;
-            if(tmp.getRes() == r){
+            if (tmp.getRes() == r) {
                 found = true;
                 tmp.setRes(r, q);
+                toBeRemoved = tmp;
             }
         }
-        if(!found){
+        if (!found && q != 0) {
             ResAmountComponent resAmountComponent = new ResAmountComponent();
             resAmountComponent.setRes(r, q);
             vBox.getChildren().add(resAmountComponent);
+        } else if (q == 0) {
+            vBox.getChildren().remove(toBeRemoved);
         }
     }
 
@@ -47,5 +52,11 @@ public class StrongBoxComponent extends Pane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        // example
+//        ResAmountComponent resAmountComponent = new ResAmountComponent();
+//        resAmountComponent.setRes(Resource.NOTHING, 1);
+//        vBox.getChildren().add(resAmountComponent);
+//        updateRes(Resource.NOTHING, 0);
     }
 }
