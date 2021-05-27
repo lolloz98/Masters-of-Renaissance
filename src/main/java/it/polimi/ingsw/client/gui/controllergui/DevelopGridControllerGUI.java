8f68d7bff9c76dea0025734889f5f20e3a4584ed
Aniello98 <aniello.desantis@mail.polimi.it>
2021-05-27 14:00:50 +0200

    improved developgrid.fxml

--- src/main/java/it/polimi/ingsw/client/gui/controllergui/DevelopGridControllerGUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllergui/DevelopGridControllerGUI.java
@@ -25,6 +25,21 @@ public class DevelopGridControllerGUI extends ControllerGUI implements Observer 
     public Button buydevelopBtn;
     public Button backBtn;
     public Label messageLbl;
+    public Button buyL1GreenBtn;
+    public Button buyL2GreenBtn;
+    public Button buyL3GreenBtn;
+    public Button buyL1BlueBtn;
+    public Button buyL2BlueBtn;
+    public Button buyL3BlueBtn;
+    public Button buyL1YellowBtn;
+    public Button buyL2YellowBtn;
+    public Button buyL3YellowBtn;
+    public Button buyL1PurpleBtn;
+    public Button buyL2PurpleBtn;
+    public Button buyL3PurpleBtn;
+    private Button[][] matrixBtns;
+    private boolean cardSelected;
+
 
     private static final Logger logger = LogManager.getLogger(DevelopGridControllerGUI.class);
 
@@ -54,6 +69,15 @@ public class DevelopGridControllerGUI extends ControllerGUI implements Observer 
         ui.getLocalGame().overrideObserver(this);
         ui.getLocalGame().getLocalDevelopmentGrid().overrideObserver(this);
 
+        cardSelected=false;
+
+        matrixBtns = new Button[][]{
+                {buyL1GreenBtn, buyL2GreenBtn, buyL3GreenBtn},
+                {buyL1BlueBtn, buyL2BlueBtn, buyL3BlueBtn},
+                {buyL1YellowBtn, buyL2YellowBtn, buyL3YellowBtn},
+                {buyL1PurpleBtn, buyL2PurpleBtn, buyL3PurpleBtn}
+        };
+
         backBtn.setOnMouseClicked(mouseEvent -> {
             BuildGUI.getInstance().toBoard(stage, ui);
         });
@@ -68,45 +92,35 @@ public class DevelopGridControllerGUI extends ControllerGUI implements Observer 
 
     private void updateGrid() {
         //add top cards to the grid
-        LocalDevelopCard[][] topCards=ui.getLocalGame().getLocalDevelopmentGrid().getTopDevelopCards();
-        String path;
-        Image cardImage;
-        Button button;
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        for(int i=0;i<4;i++){
-            for(int j=2;j>=0;j--){
+        LocalDevelopCard[][] topCards = ui.getLocalGame().getLocalDevelopmentGrid().getTopDevelopCards();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
                 ImageView imgView = new ImageView();
-                path = Objects.requireNonNull(classLoader.getResource(String.format("png/cards_front/%03d.png", topCards[i][2-j].getId()))).getPath();
-                File cardImageFile=new File(path);
-                cardImage=new Image(cardImageFile.toURI().toString());
-                imgView.setImage(cardImage);
+                imgView.setImage(topCards[i][j].getImage());
                 imgView.setFitHeight(218);
                 imgView.setFitWidth(167);
-                button = new Button();
                 int finalJ = j;
                 int finalI = i;
-                button.setOnMouseClicked((mouseEvent) -> {
-                    buyDevelop(topCards[finalI][2-finalJ]);
+
+                matrixBtns[i][j].setOnMouseClicked(mouseEvent -> {
+                    buyDevelop(finalI,finalJ);
                 });
-//                button.setPrefWidth(167);
-//                button.setPrefHeight(218);
-                button.setVisible(true);
-                button.setDisable(true);
-                button.setGraphic(imgView);
-//                BackgroundImage backgroundImage = new BackgroundImage( cardImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-//                Background background = new Background(backgroundImage);
-//                button.setBackground(background);
-                develop_grid.add(button,i,j);
+
+                matrixBtns[i][j].setDisable(true);
+                matrixBtns[i][j].setGraphic(imgView);
+
             }
         }
+
+
     }
 
     //activates the buttons on the grid
-    private void activateChooseCardButtons(){
-        for(Node n:develop_grid.getChildren()){
-            if(n instanceof Button){
+    private void activateChooseCardButtons() {
+        for (Node n : develop_grid.getChildren()) {
+            if (n instanceof Button) {
                 //logger.debug("the node is a button");
-                Button b=(Button) n;
+                Button b = (Button) n;
                 //logger.debug("disabling the button");
                 b.setDisable(false);
             }
@@ -114,10 +128,24 @@ public class DevelopGridControllerGUI extends ControllerGUI implements Observer 
         }
     }
 
+    public void disableOtherCardButtons(int selectedI,int selectedJ){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                if(i!=selectedI||j!=selectedJ)
+                    matrixBtns[i][j].setDisable(true);
+            }
+        }
+    }
+
     //todo
-    private void buyDevelop(LocalDevelopCard card){
-        //todo go to choose resource scene
-        //ui.getGameHandler().dealWithMessage(new BuyDevelopCardMessage(ui.getLocalGame().getGameId(),ui.getLocalGame().getMainPlayer().getId(),card.getLevel(),card.getColor(),));
+    private void buyDevelop(int i,int j) {
+        if(!cardSelected) {
+            cardSelected=true;
+            disableOtherCardButtons(i,j);
+            Platform.runLater(() -> matrixBtns[i][j].setStyle("-fx-effect: dropshadow(three-pass-box, rgba(199,17,17,0.8), 20, 0, 0, 0)"));
+            //todo go to choose resource scene
+            //ui.getGameHandler().dealWithMessage(new BuyDevelopCardMessage(ui.getLocalGame().getGameId(),ui.getLocalGame().getMainPlayer().getId(),card.getLevel(),card.getColor(),));
+        }
     }
 
 }
