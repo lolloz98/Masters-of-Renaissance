b7ac032b37dc32d28178ee7c654288187f8ae28a
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-05-29 17:26:32 +0200

    setStateOfGame4 in ManipulateGameUiTestHelper useful to set Lorenzo close to victory and to test what happens when the game is over

--- src/test/java/it/polimi/ingsw/client/ServerUiTestHelper.java
+++ src/test/java/it/polimi/ingsw/client/ServerUiTestHelper.java
@@ -86,7 +86,7 @@ class ClientHandlerUiTestHelper extends ClientHandler{
                 ControllerActionsServerSingle ca = (ControllerActionsServerSingle) controllerManager.getControllerFromMap(answerListener.getGameId());
 
                 // TO TRY DIFFERENT CONFIGURATION OF THE GAME CHANGE THIS METHOD
-                ManipulateGameUiTestHelper.setStateOfGame3(answerListener.getGameId(), ca.getGame());
+                ManipulateGameUiTestHelper.setStateOfGame4(answerListener.getGameId(), ca.getGame());
 
                 answerListener.sendAnswer(AnswerFactory.createGameStatusAnswer(ca.getGameId(), answerListener.getPlayerId(), answerListener.getPlayerId(), ca.getGame()));
             }else if((parsedMessage instanceof JoinGameMessageController) && controllerManager.getControllerFromMap(clientMessage.getGameId()).getGame() != null){
