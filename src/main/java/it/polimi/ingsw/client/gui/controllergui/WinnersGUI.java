7ddd069904da52e75896e016e1ff506c54377e48
lolloz98 <lorenzocarpaneto@yahoo.it>
2021-06-07 18:12:31 +0200

    implemented leaderBoard in gui

--- src/main/java/it/polimi/ingsw/client/gui/controllergui/WinnersGUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllergui/WinnersGUI.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.client.gui.GUI;
 import it.polimi.ingsw.client.localmodel.LocalMulti;
 import it.polimi.ingsw.client.localmodel.LocalPlayer;
 import it.polimi.ingsw.client.localmodel.LocalSingle;
+import it.polimi.ingsw.server.model.utility.PairId;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,6 +12,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 public class WinnersGUI extends ControllerGUI {
     public AnchorPane singlePlayer;
@@ -24,8 +26,8 @@ public class WinnersGUI extends ControllerGUI {
         if (ui.getLocalGame() instanceof LocalMulti) {
             singlePlayer.setVisible(false);
             int i = 1;
-            for(LocalPlayer player: ((LocalMulti) ui.getLocalGame()).getWinners()) {
-                vboxMulti.getChildren().add(new Label(i + ": id: " + player.getId() + " ; name: " + player.getName()));
+            for(PairId<LocalPlayer, Integer> entry: ((LocalMulti) ui.getLocalGame()).getLocalLeaderBoard()) {
+                vboxMulti.getChildren().add(new Label(i + ": id: " + entry.getFirst().getId() + " ; name: " + entry.getFirst().getName() + " ----> Score: " + entry.getSecond()));
                 i++;
             }
 
